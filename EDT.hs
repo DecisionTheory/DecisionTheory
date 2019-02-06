4123d12d72d349da99f69da029d9e7e62fab0ac3
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2019-02-06 10:13:00 -0200

    Reimplemented EDT to use conditioning
    Refactored code to use more appropriate types (e.g. Rational instead of Float)
    Added "XOR Blackmail" problem.
    Implemented conditioning to filter out branches that are impossible due to a given guard and renormalizes the probabilities.
    Added a causal variant for the "Newcomb" problem.

--- EDT.hs
+++ EDT.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE EmptyDataDecls, GADTs, StandaloneDeriving #-}
 
 module EDT where
-  import Data.List (groupBy, sortBy, sortOn, maximumBy, uncons, nub)
+  import Data.List (groupBy, sortBy, sortOn, maximumBy, uncons)
   import Data.Maybe (catMaybes)
   import Data.Ord (comparing)
   import Data.Function (on)
+  import qualified Data.Map.Strict as Map
+  import Data.Ratio (Rational)
 
   data Stochastic
   data Deterministic
@@ -30,9 +32,11 @@ module EDT where
   instance WithLabel (Labeled a) where
     label (Labeled s _) = s
 
-  data Probability a = Probability a Float
+  data Probability a = Probability a Rational
     deriving (Eq, Show)
-  unProbability (Probability a f) = (a, f)
+  unProbability (Probability a r) = (a, r)
+  probabilityElement (Probability a _) = a
+  probabilityValue   (Probability _ r) = r
 
   data Guard = Guard String String
     deriving (Eq, Show)
@@ -44,6 +48,8 @@ module EDT where
     deriving (Eq, Show)
 
   clauseValue (Clause _ v) = v
+
+  type Utility = Float
 
   newcomb = Graph [Labeled "action"         action
                   ,Labeled "accuracy"       accuracy
@@ -81,19 +87,22 @@ module EDT where
                                        ,Clause [Guard "outcome" "2e"]    "1000"
                                        ]
 
-  edtChoiceForNewcomb = edt (Search read "action" "value") newcomb
+  edtChoiceForNewcomb = edt [] (Search read "action" "value") newcomb
 
-  expectedEdtChoiceForNewcomb = ("onebox", 990000.0) == edtChoiceForNewcomb
+  testEdtChoiceForNewcomb = test "EdtChoiceForNewcomb" ("onebox", 990000.0) $ edtChoiceForNewcomb
 
+  testNewcombChoices = test "NewcombChoices" ["onebox", "twobox"] $ choices "action" newcomb
 
+  data Search = Search (String -> Utility) String String
 
-  data Search = Search (String -> Float) String String
-  edt :: Search -> Graph Stochastic -> (String, Float)
-  edt (Search uf a o) = consolidate . catMaybes . map expectedValue . branches
-    where expectedValue (Probability g v) = do a' <- find a g
-                                               o' <- find o g
-                                               return $ Probability (a', uf o') v
-          consolidate = maximumBy (comparing snd) . weighted
+  edt :: [Guard] -> Search -> Graph Stochastic -> (String, Utility)
+  edt gs (Search uf a o) g = maximumBy (comparing snd) $ map (\(v, ps) -> (v, sum $ map expectedValue ps)) conditions
+    where conditions :: [(String, [Probability (Graph Deterministic)])]
+          conditions = map imagine $ choices a g
+          imagine v = (v, condition (Guard a v) conditionedBranches)
+          conditionedBranches = foldl (flip condition) (branches g) gs
+          expectedValue p = maybe 0.0 ((* (fromRational $ probabilityValue p)) . uf) $ find o $ probabilityElement p
+
 
   find :: String -> Graph Deterministic -> Maybe String
   find l g@(Graph lns) = resolve =<< getByLabel lns
@@ -109,21 +118,37 @@ module EDT where
           guardMatches (Guard l' v) = find l' g == Just v
 
   probabilities :: String -> Graph Stochastic -> [Probability String]
-  probabilities l = catMaybes . map find' . branches
+  probabilities l = squash (when (==)) . catMaybes . map find' . branches
     where find' (Probability g p) = fmap (`Probability` p) (find l g)
 
-  --TODO implement squash to consolidate duplicates
+
+  when :: (a -> a -> Bool) -> (a -> a -> Maybe a)
+  when (==) a1 a2 | a1 == a2  = Just a1
+                  | otherwise = Nothing
+
+  testSquash = test "Squash" [Probability "A" 0.3, Probability "B" 0.7] $ squash (when (==)) [Probability "A" 0.1, Probability "B" 0.4, Probability "A" 0.2, Probability "B" 0.3]
+
+  test s  a b | a == b    = Left ("Ok " ++ s)
+              | otherwise = Right ("Failed " ++ s, a, b)
+
+  squash :: Ord a => (a -> a -> Maybe a) -> [Probability a] -> [Probability a]
+  squash (>+<) = squash' . sortOn unProbability
+    where squash' (p1:p2:ps) = maybe (p1:squash' (p2:ps)) (\a3 -> squash' ((Probability a3 (probabilityValue p1 + probabilityValue p2)):ps)) (probabilityElement p1 >+< probabilityElement p2)
+          squash' ps         = ps
 
   choices :: String -> Graph Stochastic -> [String]
-  choices l = nub . map (fst.unProbability) . probabilities l
+  choices l = map probabilityElement . probabilities l
 
-  weighted :: [Probability (String, Float)] -> [(String, Float)]
+  weighted :: [Probability (String, Utility)] -> [(String, Utility)]
   weighted = map weight . groupBy ((==) `on` name) . sortOn name
-    where name            = fst.fst.unProbability
-          score           = snd.fst.unProbability
-          probability     = snd.unProbability
-          weightedScore p = probability p * score p
-          weight ps       = (name $ head ps, (sum $ map weightedScore ps) / (sum $ map probability ps))
+    where name            :: Probability (String, Utility) -> String
+          name            = fst.probabilityElement
+          score           :: Probability (String, Utility) -> Utility
+          score           = snd.probabilityElement
+          weightedScore   :: Probability (String, Utility) -> Utility
+          weightedScore p = fromRational (probabilityValue p) * score p
+          weight          :: [Probability (String, Utility)] -> (String, Utility)
+          weight ps       = (name $ head ps, (sum $ map weightedScore ps) / (fromRational $ sum $ map probabilityValue ps))
 
   weird = Graph [Labeled "a" a
                 ,Labeled "b" b
@@ -140,7 +165,7 @@ module EDT where
                           ,Clause [Guard "a" "a2", Guard "b" "b1"] "c3"
                           ,Clause [Guard "a" "a2", Guard "b" "b2"] "c4"
                           ]
-  expectedProbabilitiesForWeird = [Probability "c1" 0.5, Probability "c4" 0.5] == probabilities "c" weird
+  testProbabilitiesForWeird = test "ProbabilitiesForWeird" [Probability "c1" 0.5, Probability "c4" 0.5] $ probabilities "c" weird
 
   weirder = [Probability (Graph [Labeled "a" $ Always "a1"
                                 ,Labeled "b" $ Conditional [Clause [Guard "a" "a1"] "b1"]
@@ -166,7 +191,7 @@ module EDT where
           loop ((Labeled l (Conditional c)):ls) = map (prepend (Labeled l (Conditional c))) $ loop ls
           prepend l (Probability (Graph ls) p) = Probability (Graph (l:ls)) p
 
-  expectedWeirdBranches = expected == actual
+  testWeirdBranches = test "WeirdBranches" expected actual
     where actual = branches weird
           expected = [Probability (Graph [Labeled "a" (Always "a1")
                                          ,Labeled "b" (Conditional [Clause [Guard "a" "a1"] "b1",Clause [Guard "a" "a2"] "b2"])
@@ -178,7 +203,7 @@ module EDT where
                                          ]) 0.5
                      ]
 
-  expectedBranches = expected == actual
+  testBranches = test "Branches" expected actual
     where actual = branches (Graph [Labeled "a" $ Distribution [Probability "a1" 0.1
                                                                ,Probability "a2" 0.9
                                                                ]
@@ -186,8 +211,112 @@ module EDT where
                                                                ,Probability "b2" 0.7
                                                                ]
                                    ])
-          expected = [Probability (Graph [Labeled "a" (Always "a1"),Labeled "b" (Always "b1")]) 3.0000001e-2
+          expected = [Probability (Graph [Labeled "a" (Always "a1"),Labeled "b" (Always "b1")]) 0.03
                      ,Probability (Graph [Labeled "a" (Always "a1"),Labeled "b" (Always "b2")]) 0.07
                      ,Probability (Graph [Labeled "a" (Always "a2"),Labeled "b" (Always "b1")]) 0.27
                      ,Probability (Graph [Labeled "a" (Always "a2"),Labeled "b" (Always "b2")]) 0.63
                      ]
+
+
+  causalNewcomb = Graph [Labeled "action"         action
+                        ,Labeled "accuracy"       accuracy
+                        ,Labeled "prediction"     prediction
+                        ,Labeled "predisposition" predisposition
+                        ,Labeled "box_b"          box_b
+                        ,Labeled "outcome"        outcome
+                        ,Labeled "value"          value
+                        ]
+    where predisposition = Distribution [Probability "oneboxer" 0.5
+                                        ,Probability "twoboxer" 0.5
+                                        ]
+          action         = Conditional [Clause [Guard "predisposition" "oneboxer"] "onebox"
+                                       ,Clause [Guard "predisposition" "twoboxer"] "twobox"
+                                       ]
+          accuracy       = Distribution [Probability "accurate"   0.99
+                                        ,Probability "inaccurate" 0.01
+                                        ]
+          prediction     = Conditional [Clause [Guard "predisposition" "oneboxer", Guard "accuracy"   "accurate"] "p1"
+                                       ,Clause [Guard "predisposition" "twoboxer", Guard "accuracy"   "accurate"] "p2"
+                                       ,Clause [Guard "predisposition" "oneboxer", Guard "accuracy" "inaccurate"] "p2"
+                                       ,Clause [Guard "predisposition" "twoboxer", Guard "accuracy" "inaccurate"] "p1"
+                                       ]
+          box_b          = Conditional [Clause [Guard "prediction" "p1"] "full"
+                                       ,Clause [Guard "prediction" "p2"] "empty"
+                                       ]
+          outcome        = Conditional [Clause [Guard "action" "onebox", Guard "box_b" "full" ] "1f"
+                                       ,Clause [Guard "action" "twobox", Guard "box_b" "full" ] "2f"
+                                       ,Clause [Guard "action" "onebox", Guard "box_b" "empty"] "1e"
+                                       ,Clause [Guard "action" "twobox", Guard "box_b" "empty"] "2e"
+                                       ]
+          value          = Conditional [Clause [Guard "outcome" "1f"] "1000000"
+                                       ,Clause [Guard "outcome" "2f"] "1001000"
+                                       ,Clause [Guard "outcome" "1e"]       "0"
+                                       ,Clause [Guard "outcome" "2e"]    "1000"
+                                       ]
+
+  edtChoiceForCausalNewcomb = edt [] (Search read "action" "value") causalNewcomb
+
+  testEdtChoiceForCausalNewcomb = test "EdtChoiceForCausalNewcomb" ("onebox", 990000.0) $ edtChoiceForCausalNewcomb
+
+  testCausalNewcombChoices = test "CausalNewcombChoices" ["onebox", "twobox"] $ choices "action" newcomb
+
+  xorBlackmail = Graph [Labeled "infestation" infestation
+                       ,Labeled "prediction"  prediction
+                       ,Labeled "action"      action
+                       ,Labeled "value"       value
+                       ,Labeled "observation" observation
+                       ]
+    where infestation = Distribution [Probability "termites" 0.5
+                                     ,Probability "no termites" 0.5
+                                     ]
+          prediction  = Conditional [Clause [Guard "infestation" "termites",    Guard "action" "pay"   ] "skeptic"
+                                    ,Clause [Guard "infestation" "termites",    Guard "action" "refuse"] "gullible"
+                                    ,Clause [Guard "infestation" "no termites", Guard "action" "pay"   ] "gullible"
+                                    ,Clause [Guard "infestation" "no termites", Guard "action" "refuse"] "skeptic"
+                                    ]
+          observation = Conditional [Clause [Guard "prediction" "gullible"] "letter"
+                                    ,Clause [Guard "prediction" "skeptic"]  "no letter"
+                                    ]
+          action      = Distribution [Probability "pay" 0.5
+                                     ,Probability "refuse" 0.5
+                                     ]
+          value       = Conditional [Clause [Guard "infestation" "termites",    Guard "action" "pay"   ] "-1001000"
+                                    ,Clause [Guard "infestation" "termites",    Guard "action" "refuse"] "-1000000"
+                                    ,Clause [Guard "infestation" "no termites", Guard "action" "pay"   ] "-1000"
+                                    ,Clause [Guard "infestation" "no termites", Guard "action" "refuse"] "-0"
+                                    ]
+
+  edtChoiceForXorBlackmail = edt [Guard "observation" "letter"] (Search read "action" "value") xorBlackmail
+
+  testEdtChoiceForXorBlackmail = test "EdtChoiceForXorBlackmail" ("pay", -1000.0) $ edtChoiceForXorBlackmail
+
+  -- TODO refactor condition' to use filter
+  condition :: Guard -> [Probability (Graph Deterministic)] -> [Probability (Graph Deterministic)]
+  condition (Guard l v) = normalize . condition'
+    where condition' :: [Probability (Graph Deterministic)] -> [Probability (Graph Deterministic)]
+          condition' [] = []
+          condition' (p:ps) = case conditionBranch (probabilityElement p) of
+                                Just g  -> Probability g (probabilityValue p) : condition' ps
+                                Nothing -> condition' ps
+          conditionBranch :: Graph Deterministic -> Maybe (Graph Deterministic)
+          conditionBranch g | Just v == find l g = Just g
+                            | otherwise          = Nothing
+
+  normalize :: [Probability a] -> [Probability a]
+  normalize ps = normalize' (sum (map probabilityValue ps)) ps
+    where normalize' _ []     = []
+          normalize' t (p:ps) = Probability (probabilityElement p) (probabilityValue p / t) : normalize' t ps
+
+  testNormalize =  test "Normalize" [Probability "A" 0.25,Probability "B" 0.75] $ normalize [Probability "A" 0.1, Probability "B" 0.3]
+
+  tests = (testEdtChoiceForNewcomb
+          ,testNewcombChoices
+          ,testSquash
+          ,testProbabilitiesForWeird
+          ,testWeirdBranches
+          ,testBranches
+          ,testEdtChoiceForCausalNewcomb
+          ,testCausalNewcombChoices
+          ,testNormalize
+          ,testEdtChoiceForXorBlackmail
+          )
