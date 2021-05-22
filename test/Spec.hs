e884844a9bfd3d023cefe707b4508c198cf19148
Gustavo Bicalho <gusbicalho@gmail.com>
2021-05-22 18:19:13 +0000

    port Parfits and DeathInDamascus tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,9 @@ import qualified V2.Newcomb as V2.NP
 import qualified XorBlackmail as XB
 import qualified V2.XorBlackmail as V2.XB
 import qualified DeathInDamascus as DiD
+import qualified V2.DeathInDamascus as V2.DiD
 import qualified ParfitsHitchhiker as PH
+import qualified V2.ParfitsHitchhiker as V2.PH
 
 main :: IO ()
 main = do DT.tests
@@ -14,3 +16,5 @@ main = do DT.tests
           PH.tests
           V2.NP.tests
           V2.XB.tests
+          V2.DiD.tests
+          V2.PH.tests
